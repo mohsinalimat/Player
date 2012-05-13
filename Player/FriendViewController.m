64e1093c4f2ee376b81718ab94de88894bb6647e
Sina Jazayeri <sina@sinjaz.com>
2012-05-13 15:20:02 -0400

    FriendViewController added

--- Player/FriendViewController.m
+++ Player/FriendViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "FriendViewController.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface FriendViewController ()
 
@@ -25,9 +26,32 @@
 
 -(void)displayFriendInfo:(Friend*)friend
 {
-    NSLog(@"%@", friend.name);
-    UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(20, 20, 100, 100)];
-    label.text = friend.name;
+    NSURL *imageURL = [NSURL URLWithString:[friend imageURL_iPad]];
+    NSData *imageData = [NSData dataWithContentsOfURL:imageURL];
+    UIImage *image = [UIImage imageWithData:imageData];
+    
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(20,20,200,200)];
+    imageView.layer.masksToBounds = YES;
+    imageView.layer.cornerRadius = 10;
+    imageView.contentMode = UIViewContentModeScaleAspectFit;
+    imageView.image = image;
+    imageView.backgroundColor = [UIColor blackColor];
+    [self.view addSubview:imageView];
+    
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(240, 20, 400, 100)];
+    //label.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    label.text = [friend name];
+    label.textAlignment = UITextAlignmentLeft;
+    label.backgroundColor = [UIColor clearColor];
+    label.textColor = [UIColor blackColor];
+    label.highlightedTextColor = [UIColor grayColor];
+    //if (INTERFACE_IS_PHONE)
+    //   label.font = [UIFont boldSystemFontOfSize:10];
+    //else
+        label.font = [UIFont boldSystemFontOfSize:30];
+    //label.shadowColor = [UIColor blackColor];
+    //label.shadowOffset = CGSizeMake(1, 2);
+    [self.view addSubview:label];
 }
 
 - (void)viewDidLoad
